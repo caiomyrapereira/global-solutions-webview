c90680d043e3ba266ae60fd2b6870514e5bffc98
caio myra pereira <caiomyrapereira@hotmail.com>
2023-06-04 21:08:25 -0300

    ajuste na tela de painel

--- src/pages/painel/Painel.js
+++ src/pages/painel/Painel.js
@@ -1,9 +1,36 @@
 import './Painel.css';
-
+import Alertas from '../../fontes/alertas.svg';
+import Producao from '../../fontes/producao.svg';
 function Painel() {
   return (
     <div className="App">
-      <p>Painel</p>
+      <h1>Painel de controle</h1>
+      <h2>Produção de culturas</h2>
+      <div>
+        <p>Volume total produzido</p>
+        <p>Rendimento Médio</p>
+        <p>Áreas de Cultivo</p>
+        <p>Variedade de Culturas</p>
+      </div>
+      <h2>Estoques de alimentos</h2>
+      <div>
+        <p>482,7 mil toneladas</p>
+        <p>Em sua região
+          (Sudeste - Brasil)</p>
+        <a>Ver tipos</a>
+      </div>
+      <div>
+        <p>Regiões nacionais</p>
+        <p>Mundial</p>
+      </div>
+
+      <h2>Alertas</h2>
+
+      <img src={Alertas} className='w-100' alt='logo' />
+
+      <h2>Tendências de Produção</h2>
+      <img src={Producao} className='w-100' alt='logo' />
+
     </div>
   );
 }
